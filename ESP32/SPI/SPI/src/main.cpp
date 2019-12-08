b82aa6bc41710b9757c21c3ea34ee8d083620ca8
Thomas Huster <tehuster@gmail.com>
2019-12-08 12:57:41 +0100

    ESP SPI

---
+++ ESP32/SPI/SPI/src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include <SPI.h>
+uint8_t val;
+
+#define SETLED 0
+#define SHOWLEDS 1
+
+void SetLed(int ledIndex, int r, int g, int b);
+void ShowLeds();
+
+void setup()
+{
+    pinMode(13, OUTPUT);
+    pinMode(13, HIGH);
+
+    pinMode(32, OUTPUT);
+    pinMode(34, INPUT);
+    Serial.begin(115200);   //set baud rate to 115200 for usart
+    digitalWrite(32, HIGH); // disable Slave Select
+
+    //10 (SS), 11 (MOSI), 12 (MISO), 13 (SCK)
+    //32     , 15       , 16         14
+    SPI.begin(14, 16, 15, 32);
+    SPI.setClockDivider(SPI_CLOCK_DIV32);
+    Serial.println("Enter characters to transfer");
+}
+
+void loop()
+{
+    int buttonPress = digitalRead(34);
+    
+    if(buttonPress)
+    {
+    for(int i = 0; i < 10; i++)
+    {
+        SetLed(i, 255, 0, 0);
+        ShowLeds();
+    }
+
+    delay(10);
+
+    for(int i = 0; i < 10; i++)
+    {
+        SetLed(i, 0, 255, 0);
+        ShowLeds();
+    }    
+    delay(10);
+    }
+}
+
+
+void SetLed(int ledIndex, int r, int g, int b)
+{
+    digitalWrite(32, LOW); // enable Slave Select
+    SPI.transfer(SETLED); 
+    SPI.transfer(ledIndex);    
+    SPI.transfer(r);    
+    SPI.transfer(g); 
+    SPI.transfer(b); 
+    SPI.transfer('\n');   
+    digitalWrite(32, HIGH); // disable Slave Select
+    delay(1);               // Wait for Ledstrip, do we need this?
+}
+
+void ShowLeds()
+{
+    digitalWrite(32, LOW); // enable Slave Select
+    SPI.transfer(SHOWLEDS); 
+    SPI.transfer('\n');   
+    digitalWrite(32, HIGH); // disable Slave Select
+    delay(1);               // Wait for Ledstrip, do we need this?
+}
